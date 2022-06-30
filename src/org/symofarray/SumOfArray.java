63d92487f2240ff234277539736d0c2ebc12777e
Jayaprakash29 <psathya428@gmail.com>
2022-06-30 11:21:21 +0530

    Sumofarry

---
+++ src/org/symofarray/SumOfArray.java
@@ -0,0 +1,14 @@
+package org.symofarray;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int _array[] = {1,2,3,4,5,6,7,8,9,10};
+		int sum = 0;
+		for(int i: _array )
+			sum += i;
+		System.out.println("The sum is " + sum);
+		
+		
+	}
+}
